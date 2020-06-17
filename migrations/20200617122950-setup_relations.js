544f2a2f0abdb8a61e964cfbc7e0357670ed9306
Moises Bessalle <mbessalle@gmail.com>
2020-06-17 15:12:13 +0200

    migration and seeder files

---
+++ migrations/20200617122950-setup_relations.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const artwork = require("../models/artwork");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("artworks", "userId", {
+      type: Sequelize.INTEGER,
+      reference: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+    await queryInterface.addColumn("bids", "artworkId", {
+      type: Sequelize.INTEGER,
+      reference: {
+        model: "artworks",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+    await queryInterface.addColumn(
+      "users",
+      "isArtist",
+      { type: Sequelize.BOOLEAN },
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("artworks", "userId");
+    await queryInterface.removeColumn("bids", "artworkId");
+  },
+};

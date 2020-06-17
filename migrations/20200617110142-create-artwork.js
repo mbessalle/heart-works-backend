3ed770a769df2a3b07cd47c8aa44592fc2750cc1
Moises Bessalle <mbessalle@gmail.com>
2020-06-17 13:16:32 +0200

    artwork model

---
+++ migrations/20200617110142-create-artwork.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('artworks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      hearts: {
+        type: Sequelize.INTEGER
+      },
+      minimumBid: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('artworks');
+  }
+};
